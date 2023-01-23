2965d5c2d382f531778c3a76407d93d8d0989e2f
Adriano Almeida <adriano@z1.app>
2023-01-23 18:04:18 -0300

    feat: streams

---
+++ ignite-nodejs-2023/module-01/streams/buffer.js
@@ -0,0 +1,8 @@
+const buf = Buffer.from("adriano luis de almeida")
+
+console.log(buf.toJSON())
+
+
+//é uma representação de um espaço em memória para ser trafegado na aplicação
+//modo de ler em modo binário = muito mais performático
+
