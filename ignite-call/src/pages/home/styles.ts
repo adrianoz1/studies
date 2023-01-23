9b623dbc06e8bfda33d27ddf9d5a7855df31458a
Adriano Almeida <adriano@z1.app>
2023-01-23 11:02:22 -0300

    new projects

---
+++ ignite-call/src/pages/home/styles.ts
@@ -0,0 +1,34 @@
+import { styled, Heading, Text } from '@ignite-ui/react'
+
+export const Container = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20',
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  marginLeft: 'auto',
+  height: '100vh',
+})
+
+export const Hero = styled('div', {
+  maxWidth: 400,
+  padding: '0 $8',
+
+  [`> ${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`> ${Text}`]: {
+    color: '$gray200',
+  },
+})
+
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
